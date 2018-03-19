3a2c5a12acf8a1a2c7e85e6cc649035abe8abc30
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-03-19 16:46:22 -0700

    started working on Park module

--- public/scripts/models/Park.js
+++ public/scripts/models/Park.js
@@ -1,1 +1,19 @@
 'use strict';
+
+(function(module) {
+ 
+    function Park(data) {
+        Object.keys(data).forEach(key => this[key] = data[key]);
+    }
+
+    Park.all = [];
+
+    Park.populateParks = function() {
+        return $.getJSON(`${API_URL}/parks`)
+            .then(data => {
+                Park.all = data.parks.map(each => new Park(each));
+            });
+    };
+
+    module.Park = Park;
+})(window.module);
