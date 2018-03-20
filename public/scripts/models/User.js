a9b504bccf99e9bc434b52dfef7bee1f37f884e5
Ryan Manro <rmanro@gmail.com>
2018-03-20 11:20:13 -0700

    added signin code

--- public/scripts/models/User.js
+++ public/scripts/models/User.js
@@ -1,1 +1,39 @@
 'use strict';
+
+(function (module) {
+
+    const User = {};
+
+    User.current = null;
+
+    const setTokenHeader = token => {
+        $.ajaxSetup({
+            headers: { token: token }
+        });
+    };
+
+    User.signup = credentials => {
+        return $.post(`${API_URL}/auth/login`, credentials)
+            .then(response => {
+                User.current = true;
+                window.localStorage.token = response.token;
+                setTokenHeader(response.token);
+            });
+    };
+
+    User.tryToken = () => {
+        const token = window.localStorage.token;
+        if(!token) return;
+
+        setTokenHeader(token);
+        User.current = true;
+    };
+
+    User.logout = () => {
+        window.localStorage.removeItem('token');
+        User.current = false;
+    };
+
+    module.User = User; 
+
+})(window.module);
---
+++ public/scripts/views/login-view.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function(module) {
+
+    const User = module.User;
+
+    const loginView = {};
+
+    let method = '';
+
+    loginView.initSignup = () => {
+        if(User.current) {
+            $('#admin-form').hide();
+            $('#logged-in').show();
+        }
+        else {
+            method = 'signup';
+            $('#auth-type').attr('href', '/auth/login').text('Already have an account? Sign In');
+            $('#admin-form').off('submit').on('submit', handleSubmit);
+            $('#logged-in').hide();
+        }
+        $('$admin-view').show();
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const credentials = {
+            email: $('#email').val(),
+            password: $('#password').val()
+        };
+
+        User[method(credentials)]
+            .then(() => {
+                $('$admin-form')[0].reset();
+                page('/');
+            })
+            .catch(err => {
+                $('#auth-error').text(err.responseJSON.error);
+            });
+    };
+
+    module.loginView = loginView;
+
+})(window.module);
