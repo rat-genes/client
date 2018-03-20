fd5c9bba534c9dde9e5cbb4d785195e48763a67f
Victor Bofill <terranvas@gmail.com>
2018-03-19 17:09:44 -0700

    Got the parks view working

--- public/scripts/views/park-view.js
+++ public/scripts/views/park-view.js
@@ -1,8 +1,17 @@
 'use strict';
 
 (function(module) {
-    module.Park = Park;
+    const Park = module.Park;
 
-    
+    const template = Handlebars.compile($('#park-template').html());
 
+    const parkView = {};
+
+    parkView.initParkView = () => {
+        Park.all.forEach(data => {
+            $('#park-section').append(template(data));
+        })
+    };
+
+    module.parkView = parkView;
 })(window.module);
